8d8cab78e7d330aa292f0bbab23ce896180c6a98
Elmer Gonzalez <egotano@banreservas.com>
2026-04-28 01:20:31 -0400

    .

--- web/server.test.js
+++ web/server.test.js
@@ -1,3 +1,11 @@
+jest.mock('redis', () => ({
+  createClient: () => ({
+    get: (key, cb) => cb(null, null),
+    set: jest.fn()
+  })
+}));
+
+const request = require('supertest');
 const startServer = require('./server');
 
 describe('Server', () => {
@@ -5,11 +13,10 @@ describe('Server', () => {
     it('should start and respond on a port', async () => {
         const { app, server } = startServer(0);
 
-        const request = require('supertest');
-
         const res = await request(app).get('/');
 
         expect(res.statusCode).toBe(200);
+        expect(res.text).toContain('Number of visits is');
 
         server.close();
     });
